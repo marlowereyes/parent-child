341951a3d8d598c8577f06268808bd6f39167ff6
MarloweReyes <mreyes58@my.bcit.ca>
2024-04-22 08:36:45 -0700

    font pt2

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-// app/layout.jsx
-import { lexend } from '../fonts'
+// app/layout.js or app/layout.jsx
+import { Lexend } from 'next/font/google'
+
+const lexend = Lexend({
+  subsets: ['latin'],
+  weight: ['400', '600', '700']
+})
 
 export default function RootLayout({ children }) {
   return (
